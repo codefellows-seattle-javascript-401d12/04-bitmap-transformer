beb1e2b9820273de48c1d01260ba1af3e7bab0b8
Kyler Dotson <kyler.dotson@gmail.com>
2016-12-09 17:53:30 -0800

    basic framework of bitmap transformer library, gulpfile, package.json

--- bitmap-team0/index.js
+++ bitmap-team0/index.js
@@ -1,2 +1,15 @@
+'use strict'
 
-hello
+const TB = require('./lib/TransformableBitmap.js');
+
+var ourTB = new TB;
+
+ourTB.read('data/thebitmapfile.bmp');
+
+ourTB.greyscale();
+
+ourTB.invert();
+
+ourTB.increaseBrightness(50);
+
+ourTB.write('the output transformed bitmap file');
