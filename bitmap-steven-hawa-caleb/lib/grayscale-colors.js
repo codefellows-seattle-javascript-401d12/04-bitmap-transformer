a2f1dac3eea4433c7cf99d89d7c32714681e9871
Hawa Abdi <abdih17@hotmail.com>
2016-12-12 11:45:24 -0800

    Finished writing tests for the grayscale transformers, and edited the grayscale-colors.js file.

--- bitmap-steven-hawa-caleb/lib/grayscale-colors.js
+++ bitmap-steven-hawa-caleb/lib/grayscale-colors.js
@@ -1,27 +1,15 @@
 'use strict';
 
 const fs = require('fs');
-const bmp = require(`${__dirname}/../model/bitmap-constructor.js`);
-
-// const dibHeader = bmp.colorTable;
-// const dibHeader = Buffer.readInt32LE(14);
-// const startPoint = 14 + dibHeader;
-// const pixelArray = Buffer.readInt10LE(10);
-// colorTable = Buffer.slice(startPoint, pixelArray);
 
 module.exports = exports = {};
 
-//reading the bitmap image
+//Reading the bitmap image file, and storing the data collected in hex decimal values into the transformedBuffer variable. There are 4 bytes per pixel (RGBA). The transformerBuffer variable is collecting the raw buffer data from each set. In order to find the grayscale value, I looped through every 4 bytes of RGBA set and found the average of the RGB values only- excluding the alpha because it deals with transparency, and storing that into the grayscale variable. Then, I am using the writeUInt8 method ( writeUInt8(value, offset) ) to store the grayscale values and each set (4 bytes) of the collected buffer data. Lastly, we are writing the new values into the bitmap image file I created called ../img/grayscale-bitmap.bmp.
 exports.grayscaleTransform = function(callback) {
   fs.readFile(`${__dirname}/../../img/palette-bitmap.bmp`, function(err, data) {
-    //storing raw data (0s and 1s)
     var transformedBuffer = Buffer.from(data);
-    //the value in which I am computing
     var grayValue;
-    // looping through each data (bytes which is 4 bytes)
     for (var i = 54; i < 1078; i+=4) {
-      // there are 4 bytes for a pixel (r,g,b,alpha) - alpha not included
-      // taking the average - alpha not required for gray scale
       grayValue = (transformedBuffer.readUInt8(i) + transformedBuffer.readUInt8(i+1) + transformedBuffer.readUInt8(i+2))/3;
       transformedBuffer.writeUInt8(grayValue, i);
       transformedBuffer.writeUInt8(grayValue, i+1);
--- bitmap-steven-hawa-caleb/test/grayscale-test.js
+++ bitmap-steven-hawa-caleb/test/grayscale-test.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const fs = require('fs');
-const createBmp = require(`${__dirname}/../model/bitmap-constructor.js`);
-const inverter = require(`${__dirname}/../lib/grayscale-colors.js`);
+const grayscaleColors = require(`${__dirname}/../lib/grayscale-colors.js`);
 const expect = require('chai').expect;
 
 describe('Grayscale Manipulation', function() {
   describe('#grayscaleTransform', function() {
     it('Should transform data into a grayscale.', function(done) {
-      inverter.grayscaleTransform(function(err, buffer) {
+      grayscaleColors.grayscaleTransform(function(err, buffer) {
         var checkOutliers = false;
-        for (var i = 54; i < 1078; i++) {
+        for (var i = 54; i < 1078; i+=4) {
           if (buffer[i] > 255 || buffer[i] < 0) checkOutliers = true;
         }
         expect(checkOutliers).to.equal(false);
@@ -18,9 +17,21 @@ describe('Grayscale Manipulation', function() {
       });
     });
 
+    it('Should have a similar color value', function(done) {
+      grayscaleColors.grayscaleTransform(function (err, TransformBuffer) {
+        var checkData = true;
+        for (var i = 54; i < 1078; i+=4){
+          var grayValue = (TransformBuffer.readUInt8(i) + TransformBuffer.readUInt8(i+1) + TransformBuffer.readUInt8(i+2))/3;
+          if (TransformBuffer[i] != grayValue || TransformBuffer[i+1] != grayValue || TransformBuffer[i+2] !== grayValue) checkData = false;
+        }
+        expect(checkData).to.equal(true);
+        done();
+      });
+    });
+
     describe('#createGreyscaleBitmap', function() {
       it('Should create a file called grayscale-palette.bmp', function(done) {
-        inverter.createGrayscaleBitmap(function() {
+        grayscaleColors.createGrayscaleBitmap(function() {
           fs.readFile(`${__dirname}/../../img/grayscale-bitmap.bmp`, function(err, data) {
             expect(err).to.equal(null);
             expect(data).to.have.length.above(0);
