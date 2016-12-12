664f075b03cb814f4a8bd7cdb7d23ee98210676d
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-12 11:40:42 -0800

    add cli handler for bluescale transform app

--- bitmap-steven-hawa-caleb/bitmap-transformer.js
+++ bitmap-steven-hawa-caleb/bitmap-transformer.js
@@ -3,6 +3,7 @@
 const cliInterpreter = require('./lib/cli-interpreter.js');
 const invert = require('./lib/invert-colors.js');
 const grayscale = require('./lib/grayscale-colors.js');
+const bluescale = require('./lib/bluescale-colors.js');
 
 const transformArray = cliInterpreter(process.argv);
 
@@ -15,5 +16,11 @@ if (transformArray.some(function(element) { return element.toLowerCase() === 'in
 if (transformArray.some(function(element) { return element.toLowerCase() === 'grayscale'; })) {
   grayscale.createGrayscaleBitmap(function() {
     console.log('Grayscale colors of palette-bitmap.bmp.');
+  });
+}
+
+if (transformArray.some(function(element) { return element.toLowerCase() === 'bluescale'; })) {
+  bluescale.createBlueScaleBMP(function() {
+    console.log('Blue-scaled the colors of palette-bitmap.bmp.');
   });
 }
