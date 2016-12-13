5c6c2960ad83c9beec4247844ac6472cff0cee20
Kyler Dotson <kyler.dotson@gmail.com>
2016-12-12 18:59:21 -0800

    offset to palette is now determined by header data

--- bitmap-team0/lib/TransformableBitmap.js
+++ bitmap-team0/lib/TransformableBitmap.js
@@ -1,20 +1,20 @@
 'use strict';
 
-const colorTableOffset = 54;
-
 const fs = require('fs');
 
 module.exports = TransformableBitmap;
 
 function TransformableBitmap(path) {
 	this.bitmap = fs.readFileSync(path);
-	var colorTableOffset = 14 + this.bitmap.readUInt32LE(14);
+	this.colorTableOffset = 14 + this.bitmap.readUInt32LE(14);
+	//this.width = this.bitmap.readUInt32LE(18);
+	//this.height = this.bitmap.readUInt32LE(22);
 };
 
 TransformableBitmap.prototype.invert = function() {
 	for (var i = 0; i <= 255; i++) {
 		for (var j = 0; j <= 2; j++) {
-			this.bitmap[colorTableOffset + i * 4 + j] = 255 - this.bitmap[colorTableOffset + i * 4 + j];
+			this.bitmap[this.colorTableOffset + i * 4 + j] = 255 - this.bitmap[this.colorTableOffset + i * 4 + j];
 		}
 	}
 	return this;
